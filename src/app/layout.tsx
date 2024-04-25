faab1ab93d208b826bae671d494e013a34244853
MYSELF-SAYAN <116438215+MYSELF-SAYAN@users.noreply.github.com>
2024-04-25 19:22:41 +0530

    vercel analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 import {AppWrapper} from "../context/index"
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <AppWrapper>
         {children}
+        <Analytics />
         </AppWrapper>
         </body>
     </html>
